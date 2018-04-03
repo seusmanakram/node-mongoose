7646aef78cda0fb1b53a4a91e8469cb584630d1a
Usman Akram <usman.akram1996@outlook.com>
2018-04-03 22:01:36 +0500

    Mongoose Part 3

--- index.js
+++ index.js
@@ -19,11 +19,28 @@ connect.then((db) => {
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                $set: {description: 'Updated test'},
+            },
+                {
+                    new: true
+            })
+            
+            .exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting it feel!',
+                author:'Leonardo'
+            });
 
+            return dish.save();
+        })
+
+        .then((dish) => {
+            console.log(dish);
             return db.collection('dishes').drop();
         })
         .then(() => {
